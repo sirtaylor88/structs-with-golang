9acf18e0ce1eff4d38fa3ea526470b3b8fd227f5
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-22 14:46:19 +0100

    Add structs with receiver functions.

--- main.go
+++ main.go
@@ -35,5 +35,15 @@ func main() {
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	jim.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
