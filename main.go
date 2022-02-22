dd113937cc86d3afb1648a2c472c1edd0ab5d184
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-22 14:36:57 +0100

    Add embeded structs.

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -12,12 +17,23 @@ func main() {
 
 	// alex := person{firstName: "Alex", lastName: "Anderson"}
 
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	// var alex person
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 
-	fmt.Println(alex)
+	// fmt.Println(alex)
 
 	// Print the field values of struct "alex"
-	fmt.Printf("%+v", alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 69008,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
 }
